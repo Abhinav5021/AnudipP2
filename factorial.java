7e1e897977cc026687369ea14336ba956c8e1948
Abhinav5021 <abhinavspawar2005@gmail.com>
2026-01-12 10:40:34 +0530

    fact

--- factorial.java
+++ factorial.java
@@ -1,6 +1,6 @@
 public class factorial{
-    public static void main(){
-        int num = 50;
+    public static void main(String[] args){
+        int num = 5;
         long fact = 1;
         for(int i=1;i<=num;i++){
             fact = fact * i;
